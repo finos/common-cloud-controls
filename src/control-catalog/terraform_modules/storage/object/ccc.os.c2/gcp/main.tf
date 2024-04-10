9a92f7b414701094ac244c7615ce4dc534248ef3
Damien <damien.burks@citi.com>
2024-04-10 10:27:28 -0500

    updating ccc.os.c2's variable name

--- src/control-catalog/terraform_modules/storage/object/ccc.os.c2/gcp/main.tf
+++ src/control-catalog/terraform_modules/storage/object/ccc.os.c2/gcp/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "malicious_storage_bucket" {
-  name          = "${var.bucket_name}-ccc-os-c1"
+  name          = "${var.bucket_name}-ccc-os-c2"
   location      = "US"
   force_destroy = true
 
