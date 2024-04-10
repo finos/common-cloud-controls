590c4b43247cebad3877645a5fe830fc83bb23c4
mlysaght <michael.anthony.lysaght@citi.com>
2024-04-10 14:04:08 +0200

    Add in additional header and black format

--- src/control-catalog/behave_tests/ccc.os.c2/steps/ccc_os_c2.py
+++ src/control-catalog/behave_tests/ccc.os.c2/steps/ccc_os_c2.py
@@ -9,21 +9,30 @@ logging.basicConfig(level=logging.DEBUG)
 STORAGE_BUCKET_NAME = "malicious-sb-ccc-os-c2"
 UNTRUSTED_KEY_ALIAS = "alias/malicious-sb-untrusted-ccc-os-c2"
 
+
 @given("you own the object storage bucket in AWS")
 def verify_aws_bucket_exists(context):
     context.s3_client = boto3.client("s3")
     context.s3_client.get_bucket_policy(Bucket=STORAGE_BUCKET_NAME)
 
 
-@when("a data plane request with an untrusted KMS key is made to the object storage bucket")
+@when(
+    "a data plane request with an untrusted KMS key is made to the object storage bucket"
+)
 def upload_obj_with_untrusted_key(context):
     context.kms_client = boto3.client("kms")
-    untrusted_key_arn = context.kms_client.describe_key(KeyId=UNTRUSTED_KEY_ALIAS)["KeyMetadata"]["Arn"]
+    untrusted_key_arn = context.kms_client.describe_key(KeyId=UNTRUSTED_KEY_ALIAS)[
+        "KeyMetadata"
+    ]["Arn"]
     object_key = "test_obj"
     object_content = b"Hello, world!"
     try:
         context.s3_client.put_object(
-            Bucket=STORAGE_BUCKET_NAME, Key=object_key, Body=object_content, SSEKMSKeyId=untrusted_key_arn
+            Bucket=STORAGE_BUCKET_NAME,
+            Key=object_key,
+            Body=object_content,
+            SSEKMSKeyId=untrusted_key_arn,
+            ServerSideEncryption="aws:kms",
         )
     except ClientError as err:
         context.s3_publish_error = str(err)
