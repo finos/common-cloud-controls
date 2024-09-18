607666fd5c4ca66533a7d45f88e18dad42857b65
Eddie Knight <knight@linux.com>
2024-09-18 15:41:02 -0500

    cleaned up cli output
    Signed-off-by: Eddie Knight <knight@linux.com>

--- delivery-tooling/gen-markdown.go
+++ delivery-tooling/gen-markdown.go
@@ -16,13 +16,20 @@ var (
 		Use: "md",
 		Short: "",
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			fmt.Print(divider)
+			fmt.Print(logo)
 			fmt.Println(divider)
 		},
 		PersistentPostRun: func(cmd *cobra.Command, args []string) {
 			fmt.Println(divider)
 		},
 		Run: func(cmd *cobra.Command, args []string) {
-			generateMdFile()
+			outputPath, err := generateMdFile()
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("File generated successfully: %s\n", outputPath)
+			}		
 		},
 	}
 )
@@ -31,48 +38,38 @@ func init() {
 	baseCmd.AddCommand(mdCmd)
 }
 
-func generateMdFile() {
+func generateMdFile() (string, error) {
 	checkArgs()
 
 	data := readAndCompile()
-	
-	// Generate the file
-	err := generateFileFromTemplate(data)
-	fmt.Println(err)
-	// Shove the data into a template
-
-	// Write template to MD file, and make it purrrty
-	
-	//dataYaml, err := yaml.Marshal(&data)
-	//if err != nil {
-	//	log.Fatalf("error: %v", err)
-	//}
+	return generateFileFromTemplate(data)
 }
 
-func generateFileFromTemplate(data CompiledData) error {
+func generateFileFromTemplate(data CompiledData) (outputPath string, err error) {
 	serviceName := data.Metadata.ID
 	version := data.Metadata.ReleaseDetails[len(data.Metadata.ReleaseDetails)-1].Version
+	mdFileName := fmt.Sprintf("%s_%s.md", serviceName, version)
+	outputPath = filepath.Join(viper.GetString("output-dir"), mdFileName)
 
-	mdFileName := fmt.Sprintf("%s-%s.md", serviceName, version)
-	
-	outputPath := filepath.Join(viper.GetString("output-dir"), mdFileName)
 	outputFile, err := os.Create(outputPath)
-
 	if err != nil {
-		return fmt.Errorf("error creating output file %s: %w", outputPath, err)
+		err = fmt.Errorf("error creating output file %s: %w", outputPath, err)
+		return
 	}
 	defer outputFile.Close()
 
 	templatePath := "template.md"
 	tmpl, err := template.ParseFiles(templatePath)
 	if err != nil {
-		return fmt.Errorf("error parsing template file %s: %w", templatePath, err)
+		err = fmt.Errorf("error parsing template file %s: %w", templatePath, err)
+		return
 	}
 
 	err = tmpl.Execute(outputFile, data)
 	if err != nil {
-		return fmt.Errorf("error executing template for file %s: %w", outputPath, err)
+		err = fmt.Errorf("error executing template for file %s: %w", outputPath, err)
+		return
 	}
 
-	return nil
+	return
 }
