57532b19f8add53869cfc109f2f85f254760479d
Damien Burks <20100558+damienjburks@users.noreply.github.com>
2024-09-26 08:29:25 -0500

    Artifact Release Pipeline (#426)
    Signed-off-by: Eddie Knight <knight@linux.com>
    Co-authored-by: Eddie Knight <knight@linux.com>

---
+++ delivery-tooling/catalog-compiler.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
+)
+
+type CompiledCatalog struct {
+	Metadata Metadata
+
+	// These lists contain the common and specific entries smashed together
+	Controls []Control
+	Features []Feature
+	Threats  []Threat
+
+	LatestReleaseDetails ReleaseDetails
+}
+
+type Control struct {
+	ID               string                  `yaml:"id"`
+	Title            string                  `yaml:"title"`
+	Objective        string                  `yaml:"objective"`
+	ControlFamily    string                  `yaml:"control_family"`
+	Threats          []string                `yaml:"threats"`
+	NISTCSF          string                  `yaml:"nist_csf"`
+	ControlMappings  map[string]interface{}  `yaml:"control_mappings"`
+	TestRequirements []TestRequirements      `yaml:"test_requirements"`
+}
+
+type TestRequirements struct {
+	Id        string    `yaml:"id"`
+	Text      string    `yaml:"text"`
+	TlpLevels []string  `yaml:"tlp_levels"`
+}
+
+type ControlSet struct {
+	CommonControls   []string  `yaml:"common_controls"`
+	SpecificControls []Control `yaml:"controls"`
+}
+
+// Metadata is a struct that represents the metadata.yaml file
+type Metadata struct {
+	Title              string           `yaml:"title"`
+	ID                 string           `yaml:"id"`
+	Description        string           `yaml:"description"`
+	ReleaseDetails     []ReleaseDetails `yaml:"release_details"`
+}
+
+type ReleaseDetails struct {
+	Version            string         `yaml:"version"`
+	AssuranceLevel     string         `yaml:"assurance_level"`
+	ThreatModelURL     string         `yaml:"threat_model_url"`
+	ThreatModelAuthor  string         `yaml:"threat_model_author"`
+	RedTeam            string         `yaml:"red_team"`
+	RedTeamExercizeURL string         `yaml:"red_team_exercize_url"`
+	ReleaseManager     ReleaseManager `yaml:"release_manager"`
+	ChangeLog          []string       `yaml:"change_log"`
+	Contributors       []Contributors `yaml:"contributors"`
+}
+
+type ReleaseManager struct {
+	Name      string `yaml:"name"`
+	GithubId  string `yaml:"github_id"`
+	Company   string `yaml:"company"`
+	Summary   string `yaml:"summary"`
+}
+
+type Contributors struct {
+	Name      string `yaml:"name"`
+	GithubId  string `yaml:"github_id"`
+	Company   string `yaml:"company"`
+}
+
+// FeatureSet is a struct that represents the features.yaml file
+type FeatureSet struct {
+	CommonFeatureIDs []string  `yaml:"common_features"`
+	SpecificFeatures []Feature `yaml:"features"`
+}
+
+type Feature struct {
+	ID          string `yaml:"id"`
+	Title       string `yaml:"title"`
+	Description string `yaml:"description"`
+}
+
+// ThreatSet is a struct that represents the threats.yaml file
+type ThreatSet struct {
+	CommonThreatIDs []string `yaml:"common_threats"`
+	SpecificThreats []Threat `yaml:"threats"`
+}
+
+type Threat struct {
+	ID          string   `yaml:"id"`
+	Title       string   `yaml:"title"`
+	Description string   `yaml:"description"`
+	Features    []string `yaml:"features"`
+	MITRETechnique       []string `yaml:"mitre_technique"`
+}
+
+func formatList(items []string) string {
+	result := ""
+	for i, item := range items {
+		if i > 0 {
+			result += ", "
+		}
+		result += item
+	}
+	return result
+}
+
+func getDataDirectory(name string) (string) {
+	buildTarget := filepath.Join(viper.GetString("services-dir"), viper.GetString("build-target"))
+	serviceDir := viper.GetString("services-dir")
+
+	switch name {
+	case "controls":
+		return buildTarget
+	case "features":
+		return buildTarget
+	case "threats":
+		return buildTarget
+	case "metadata":
+		return buildTarget
+	case "common-controls":
+		return serviceDir
+	case "common-features":
+		return serviceDir
+	case "common-threats":
+		return serviceDir
+	default:
+		log.Fatalf("error: %v", "Invalid data type")
+	}
+	return ""
+}
+
+func readYamlFile(filepath string) (yamlFile []byte) {
+	yamlFile, err := os.ReadFile(filepath)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return
+}
+
+func getYaml(name string) ([]byte) {
+	directory := getDataDirectory(name)
+	return readYamlFile(fmt.Sprintf("%s/%s.yaml", directory, name))
+}
+
+func unmarshalData(dataName string, dataSet interface{}) {
+	yamlData := getYaml(dataName)
+    err := yaml.Unmarshal(yamlData, dataSet)
+    if err != nil {
+        log.Fatalf("error reading %s: %v", dataName, err)
+    }
+}
+
+func readAndCompileCatalog() (data CompiledCatalog) {
+	// read controls.yaml, features.yaml, threats.yaml, and metadata.yaml from dir path
+	controlsData := ControlSet{}
+	unmarshalData("controls", &controlsData)
+	featuresData := FeatureSet{}
+	unmarshalData("features", &featuresData)
+	threatsData := ThreatSet{}
+	unmarshalData("threats", &threatsData)
+	metadata := Metadata{}
+	unmarshalData("metadata", &metadata)
+
+	// read the common controls, features, and threats from the common entries directory
+	commonControlsData := ControlSet{}
+	unmarshalData("common-controls", &commonControlsData)
+	commonFeaturesData := FeatureSet{}
+	unmarshalData("common-features", &commonFeaturesData)
+	commonThreatsData := ThreatSet{}
+	unmarshalData("common-threats", &commonThreatsData)
+
+	return CompiledCatalog{
+		Metadata: metadata,
+		Controls: append(commonControlsData.SpecificControls, controlsData.SpecificControls...),
+		Features: append(commonFeaturesData.SpecificFeatures, featuresData.SpecificFeatures...),
+		Threats:  append(commonThreatsData.SpecificThreats, threatsData.SpecificThreats...),
+		LatestReleaseDetails: metadata.ReleaseDetails[len(metadata.ReleaseDetails)-1],
+	}
+}
---
+++ delivery-tooling/templates/catalog.md
@@ -0,0 +1,104 @@
+<!-- markdownlint-disable -->
+# {{ .Metadata.ID }} v{{ .LatestReleaseDetails.Version }} ({{ .Metadata.Title }})
+
+<img height="250px" src="https://github.com/finos/branding/blob/master/project-logos/active-project-logos/FINOS%20Common%20Cloud%20Controls%20Logo/Horizontal/2023_FinosCCC_Horizontal.svg?raw=true" alt="CCC Logo"/>
+
+{{ .Metadata.Description }}
+
+## Release Notes
+
+> _{{ .LatestReleaseDetails.ReleaseManager.Summary }}_
+
+Release Manager - **{{ .LatestReleaseDetails.ReleaseManager.Name }}, {{ .LatestReleaseDetails.ReleaseManager.Company }}** ([{{ .LatestReleaseDetails.ReleaseManager.GithubId }}](https://github.com/{{ .LatestReleaseDetails.ReleaseManager.GithubId }}))
+
+## Changes Since Last Release
+{{ range .LatestReleaseDetails.ChangeLog }}
+- {{ . }}
+{{- end }}
+
+## Features
+
+|Feature ID|Feature Title|
+|----|----|
+{{- range .Features }}
+|{{ .ID }}|{{ .Title }}|
+{{- end }}
+
+---
+{{ range .Features }}
+### {{ .ID }} - {{ .Title }}
+
+{{ .Description }}
+{{- end }}
+
+## Threats
+
+|Threat ID|Threat Title|
+|----|----|
+{{- range .Threats }}
+|{{ .ID }}|{{ .Title }}|
+{{- end }}
+
+---
+{{ range .Threats }}
+### {{ .ID }} - {{ .Title }}
+
+{{ .Description }}
+**Related Features:**
+{{ range .Features }}
+- {{ . }}
+{{- end }}
+
+**Related MITRE ATT&CK Values:**
+{{ range .MITRETechnique }}
+- {{ . }}
+{{- end }}
+{{ end }}
+
+## Controls
+
+|Control ID|Control Title|
+|----|----|
+{{- range .Controls }}
+|{{ .ID }}|{{ .Title }}|
+{{- end }}
+
+---
+{{ range .Controls }}
+### {{ .ID }} - {{ .Title }}
+
+{{ .Objective }}
+
+**Control Family:** {{ .ControlFamily}}
+
+**NIST CSF:** {{ .NISTCSF }}
+
+**Mitigated Threats:**
+{{ if .Threats }}
+{{ range .Threats }}
+- {{ . }}
+{{- end }}
+{{- else }}
+_No mitigated threats._
+{{- end }}
+
+**Control Mappings:**
+{{if .ControlMappings}}
+{{ range $key, $value := .ControlMappings }}
+{{- if $value }}
+{{- range $value }}
+- {{ $key }} {{ . }}
+{{- end }}
+{{- end }}
+{{- end }}
+{{else}}
+_No control mappings added._
+{{end}}
+{{ end }}
+
+## Contributing Organizations
+
+We would like to acknowledge the following organizations for their valuable contributions to this project:
+
+{{ insertSVGs }}
+<!-- markdownlint-enable -->
