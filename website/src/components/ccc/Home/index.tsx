c2dfac12c4cde3aaec238c36ff13337ca7a17d46
Rob Moffat <rob@kite9.com>
2025-09-16 08:49:06 +0100

    Component list working

--- website/src/components/ccc/Home/index.tsx
+++ website/src/components/ccc/Home/index.tsx
@@ -41,18 +41,8 @@ export default function CCCHomeTemplate({ pageData }: { pageData: HomePageData }
       <main className="container margin-vert--lg space-y-8">
         <div className="text-center">
           <h1>Common Cloud Controls</h1>
-          <p className="text-xl text-muted-foreground">All Releases</p>
+          <p className="text-xl text-muted-foreground">All Components</p>
         </div>
-        {/* <pre>{JSON.stringify(components, null, 2)}</pre> */}
-        {/* {components.map((category) =>
-          category.releases.map((release) =>
-            release.controls.map((control) => (
-              <div key={control.id}>
-                <pre>{JSON.stringify(control.control_mappings, null, 2)}</pre>
-              </div>
-            ))
-          )
-        )} */}
         <Card>
           <CardHeader>
             <CardTitle>Components Overview</CardTitle>
@@ -78,50 +68,6 @@ export default function CCCHomeTemplate({ pageData }: { pageData: HomePageData }
                     <TableCell>{comp.id}</TableCell>
                     <TableCell>{comp.numberOfReleases}</TableCell>
                     <TableCell>{comp.latestVersion}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </CardContent>
-        </Card>
-
-        <Card>
-          <CardHeader>
-            <CardTitle>All Releases</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Type</TableHead>
-                  <TableHead>Slug</TableHead>
-                  <TableHead>Version</TableHead>
-                  <TableHead>Release Manager</TableHead>
-                  <TableHead>Authors</TableHead>
-                  <TableHead>Controls</TableHead>
-                  <TableHead>Threats</TableHead>
-                  <TableHead>Capabilities</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {allReleases.map((release) => (
-                  <TableRow key={release.metadata.id}>
-                    <TableCell>{release.componentTitle}</TableCell>
-                    {/* <TableCell>
-                      <Link to={release.slug} className="text-blue-600 underline hover:text-blue-800">
-                        {release.slug}
-                      </Link>
-                    </TableCell> */}
-                    <TableCell>
-                      <Link to={release.slug} className="text-blue-600  hover:text-blue-800 hover:underline">
-                        {release.metadata.version}
-                      </Link>
-                    </TableCell>
-                    <TableCell>{release.metadata.release_details?.[0]?.["release-manager"] ? <User name={release.metadata.release_details[0]["release-manager"].name} githubId={release.metadata.release_details[0]["release-manager"]["github-id"]} company={release.metadata.release_details[0]["release-manager"].company} avatarUrl={`https://github.com/${release.metadata.release_details[0]["release-manager"]["github-id"]}.png`} /> : <span>N/A</span>}</TableCell>
-                    <TableCell>{release.metadata.release_details?.[0]?.contributors?.length || 0}</TableCell>
-                    <TableCell>{release.controls.length}</TableCell>
-                    <TableCell>{release.threats.length}</TableCell>
-                    <TableCell>{release.capabilities.length}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
