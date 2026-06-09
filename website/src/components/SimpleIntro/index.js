7eb4cc4bc8afd85bd445136d30b421b7d2a13461
Jakob Vidal <jvidal@scottlogic.com>
2026-06-09 14:42:41 +0100

    Shrank unused sidebar, moved TheStory styling to CSS file, mobile fixes

--- website/src/components/SimpleIntro/index.js
+++ website/src/components/SimpleIntro/index.js
@@ -6,17 +6,17 @@ export default function SimpleIntro() {
   return (
     <HomeSection title="Automated Governance is Within Reach.">
         <img
-    src="/img/diagrams/ccc-diagram.svg"
-    alt="CCC architecture diagram"
-    style={{
-      display: "block",
-      margin: "0 auto",
-      maxWidth: "100%",
-      width: "720px",
-      maxHeight: "340px",
-      objectFit: "contain",
-    }}
-  />
+          src="/img/diagrams/ccc-diagram.svg"
+          alt="CCC architecture diagram"
+          style={{
+            display: "block",
+            margin: "0 auto",
+            maxWidth: "100%",
+            width: "720px",
+            maxHeight: "340px",
+            objectFit: "contain",
+          }}
+        />
         <p className={styles.strap}>
           Technology-agnostic security controls for public and private cloud.
         </p>
