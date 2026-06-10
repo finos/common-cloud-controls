62afde3588ec7af22765bb895152d20709747c7c
Jakob Vidal <jvidal@scottlogic.com>
2026-06-10 14:00:21 +0100

    Added "Explore the Catalogs" buttons

--- website/src/components/SimpleIntro/index.js
+++ website/src/components/SimpleIntro/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./styles.module.css";
 import HomeSection from "../HomeSection";
+import { Link } from "react-router-dom";
 
 export default function SimpleIntro() {
   return (
@@ -20,6 +21,17 @@ export default function SimpleIntro() {
         <p className={styles.strap}>
           Technology-agnostic security controls for public and private cloud.
         </p>
+
+        <section >
+          <Link
+            to="/ccc"
+            className={styles.button}
+            onMouseEnter={(e) => { e.currentTarget.style.filter = "brightness(1.2)"; e.currentTarget.style.transform = "translateY(-1px)"; }}
+            onMouseLeave={(e) => { e.currentTarget.style.filter = "none"; e.currentTarget.style.transform = "none"; }}
+          >
+          Explore the Catalogs
+          </Link>
+        </section>
     </HomeSection>
   );
 }
