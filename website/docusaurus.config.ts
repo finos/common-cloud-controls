8bf2f452600ddf75ba0aaeeb1a11f229060f0826
Rob Moffat <rob@kite9.com>
2026-06-15 14:19:58 +0100

    Added validators page details

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -23,6 +23,12 @@ const config: Config = {
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
+  markdown: {
+    mermaid: true,
+  },
+
+  themes: ['@docusaurus/theme-mermaid'],
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
@@ -60,6 +66,7 @@ const config: Config = {
       },
       items: [
         { to: '/about', label: 'About', position: 'left' },
+        { to: '/validators', label: 'Validators', position: 'left' },
         {
           label: 'Catalogs',
           position: 'left',
