d31fd598b822ccd28878f7c3a8323071c0dfcd8d
Gabriele Balysevaite <gbalysevaite@scottlogic.com>
2026-06-17 09:32:09 +0100

    Update Contributors with leaderboards from linux foundation

--- website/src/pages/contributors.tsx
+++ website/src/pages/contributors.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import Layout from "@theme/Layout";
 import Contributors from "../components/Contributors";
 
-
 export default function ContributorHomeTemplate() {
-
   return (
     <Layout title="Contributors">
-      <Contributors/>
+      <Contributors />
     </Layout>
   );
 }
