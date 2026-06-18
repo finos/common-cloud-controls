65e265e1c8fe83df129a71f6721429134630fe92
Gabriele Balysevaite <gbalysevaite@scottlogic.com>
2026-06-18 10:20:18 +0100

    Nest Catalog navbar items

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -58,10 +58,18 @@ const config: Config = {
       },
       items: [
         { to: "/about", label: "About", position: "left" },
-        { to: "/catalogs/core", label: "CCC Catalogs", position: "left" },
-        { to: "/threats", label: "Threats", position: "left" },
-        { to: "/controls", label: "Controls", position: "left" },
-        { to: "/capabilities", label: "Capabilities", position: "left" },
+        {
+          label: "Catalogs",
+          to: "/catalogs/core",
+          position: "left",
+          type: "dropdown",
+          items: [
+            { to: "/catalogs/core", label: "CCC Catalogs" },
+            { to: "/threats", label: "Threats" },
+            { to: "/controls", label: "Controls" },
+            { to: "/capabilities", label: "Capabilities" },
+          ],
+        },
         { to: "/metadata", label: "Metadata", position: "left" },
         { to: "/cfi", label: "Test Results", position: "left" },
         {
